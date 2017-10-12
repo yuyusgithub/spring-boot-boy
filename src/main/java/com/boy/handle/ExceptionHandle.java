c2db853cf9d8f5ba81cc87b4b55ee2e675238ce1
yuyusgithub <342476212@qq.com>
2017-10-12 16:51:20 +0800

    test

---
+++ src/main/java/com/boy/handle/ExceptionHandle.java
@@ -0,0 +1,14 @@
+package com.boy.handle;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+/**
+ * Created by 彦祖 .
+ */
+
+@ControllerAdvice
+public class ExceptionHandle {
+
+
+
+}
