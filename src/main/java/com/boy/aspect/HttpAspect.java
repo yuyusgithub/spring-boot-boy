4ea592404f1fcd27979b18f7c03d7d67403693d8
yuyusgithub <342476212@qq.com>
2017-10-12 10:32:41 +0800

    test

--- src/main/java/com/boy/aspect/HttpAspect.java
+++ src/main/java/com/boy/aspect/HttpAspect.java
@@ -2,6 +2,7 @@ package com.boy.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,8 +12,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class HttpAspect {
 
-    @Before("execution(public * com.boy.controller.ValidateController.addBoy())")
-    public void log(){
+    /**
+     * 找到一个切入点，这里的切入点，就是定义时哪些类的哪些方法，关于execution表达式的格式，会在info。txt里面说明
+     */
+    @Pointcut("execution(* com.boy.controller.ValidateController.*(..))")
+    public void point(){
 
+    }
+
+    @Before("point()")
+    public void log(){
+        System.out.println("开始执行   开始   操作");
     }
 }
